a37365d18d6909b7878f823bc8f98833db37d7c1
Jakša Bašić <jaksabasic1@gmail.com>
2015-09-13 15:49:11 +0200

    Wired collection into `postsList` template

--- client/templates/posts/posts_list.js
+++ client/templates/posts/posts_list.js
@@ -1,16 +1,8 @@
 /**
  * Created by Jakša Bašić on 12/09/15.
  */
-var postsData = [
-    {
-        title: "google",
-        url: "http://google.hr"
-    },
-    {
-        title: "google",
-        url: "http://google.hr"
-    }
-];
 Template.postsList.helpers({
-    posts: postsData
+    posts: function () {
+        return Posts.find();
+    }
 });
