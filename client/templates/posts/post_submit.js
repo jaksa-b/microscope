efd53ffec2c57b6ee26df5f90a61714616a176c9
Jakša Bašić <jaksabasic1@gmail.com>
2015-09-15 14:42:56 +0200

    Added a submit post page and linked to it in the header

---
+++ client/templates/posts/post_submit.js
@@ -0,0 +1,15 @@
+/**
+ * Created by Jakša Bašić on 15/09/15.
+ */
+Template.postSubmit.events({
+    'submit form': function (e) {
+        e.preventDefault();
+
+        var post = {
+            url: $(e.target).find('[name=url]').val(),
+            title: $(e.target).find('[name=title]').val()
+        };
+        post._id = Posts.insert(post);
+        Router.go('postPage', post);
+    }
+});
