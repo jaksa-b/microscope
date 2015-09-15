57c6dc20e9f95b71baa5c3e7a1cebf19eea20f0e
Jakša Bašić <jaksabasic1@gmail.com>
2015-09-15 17:31:03 +0200

    Enforce post URL uniqueness

--- client/templates/posts/post_submit.js
+++ client/templates/posts/post_submit.js
@@ -9,11 +9,16 @@ Template.postSubmit.events({
             url: $(e.target).find('[name=url]').val(),
             title: $(e.target).find('[name=title]').val()
         };
-        Meteor.call('postInsert', post, function (error, resault) {
-            if(error){
+        Meteor.call('postInsert', post, function (error, result) {
+            // display the error to the user and abort
+            if(error)
                 return alert(error.reason);
-            }
-            Router.go('postPage', {_id:resault._id});
+
+            // show this result but route anyway
+            if (result.postExists)
+                alert('This link has already been posted');
+
+            Router.go('postPage', {_id:result._id});
         });
     }
 });
