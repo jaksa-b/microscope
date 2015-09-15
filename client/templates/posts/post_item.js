e7301e8bf4b3100c4937e4442b5e9e4878dd60cf
Jakša Bašić <jaksabasic1@gmail.com>
2015-09-15 20:59:35 +0200

    Added edit posts form

--- client/templates/posts/post_item.js
+++ client/templates/posts/post_item.js
@@ -2,6 +2,9 @@
  * Created by Jakša Bašić on 12/09/15.
  */
 Template.postItem.helpers({
+    ownPost: function() {
+        return this.userId === Meteor.userId();
+    },
     domain: function () {
         var a = document.createElement('a');
         a.href = this.url;
--- client/templates/posts/post_submit.js
+++ client/templates/posts/post_submit.js
@@ -10,7 +10,7 @@ Template.postSubmit.events({
             title: $(e.target).find('[name=title]').val()
         };
         Meteor.call('postInsert', post, function (error, result) {
-            // display the error to the user and abort
+
             if(error)
                 return alert(error.reason);
 
